f8050cd1fe2db5e6d96c346544b9c38d61577852
mkouyoumdjian <mkouyoumdjian@datanumia.com>
2023-01-30 19:42:37 +0100

    add test app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,40 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MockBuilder, MockedComponentFixture, MockRender, ngMocks} from "ng-mocks";
+import {ContactDisplayComponent} from "./modules/contact-display/contact-display.component";
+import {ContactService} from "./services/contact.service";
+import {MatDialog} from "@angular/material/dialog";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
+import {ContactEditionComponent} from "./modules/contact-edition/contact-edition.component";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterModule} from "@angular/router";
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+  let component: AppComponent;
+  let fixture: MockedComponentFixture<AppComponent>;
+
+  beforeEach(() =>
+    MockBuilder(AppComponent)
+    .mock(MatMenuModule)
+    .mock(MatToolbarModule)
+    .mock(MatIconModule)
+    .mock(RouterModule)
+  );
+
+  beforeEach(() => {
+    fixture = MockRender(AppComponent);
+    component = fixture.point.componentInstance;
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'ihm-contacts'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ihm-contacts');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ihm-contacts app is running!');
+  describe('test template', () => {
+    it('should contain router-outlet', () => {
+      expect(fixture.nativeElement.querySelector('router-outlet')).not.toBeNull();
+    });
   });
 });
